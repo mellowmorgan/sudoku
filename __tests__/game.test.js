da238cfd6e2b5fc87e365587342b80e337ca26f0
Ethan Bear <ehwbear@icloud.com>
2021-11-09 13:21:06 -0800

    Pass first test for game
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,6 +1,7 @@
+import { Game } from './../src/game.js';
 describe('Game', () => {
 
-  test('', () => {
+  test('Should create game object with 3 arrays as the properties', () => {
     const game = new Game([[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]);
     expect(game.colArray).toEqual([[1,2,3],[1,2,3]]);
     expect(game.rowArray).toEqual([[1,2,3],[1,2,3]]);
---
+++ src/game.js
@@ -0,0 +1,5 @@
+export function Game(colArray, rowArray, boxArray){
+  this.colArray = colArray;
+  this.rowArray = rowArray;
+  this.boxArray = boxArray;
+}
