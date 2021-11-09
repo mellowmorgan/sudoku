3e213ee2ad324eead938faf493b6e89fb24f6bda
Ethan Bear <ehwbear@icloud.com>
2021-11-09 15:30:46 -0800

    edit UI logic
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import { Game } from './game.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$("button").on("click", function(event){
+  event.preventDefault();
+  for(let i=1;i<10;i++){
+    for(let i2=1;i2<10;i2++){
+       let s= i.toString()+i2.toString();
+       console.log(s);
+  }}
+
+});
