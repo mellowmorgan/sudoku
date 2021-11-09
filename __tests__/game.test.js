93dc20ce6105932d2f78e36de39f2906d2a86fbc
Ethan Bear <ehwbear@icloud.com>
2021-11-09 13:52:37 -0800

    Create test 2 for isLegal method
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,10 +1,18 @@
 import { Game } from './../src/game.js';
-describe('Game', () => {
 
+describe('Game', () => {
+  let game;
+  beforeEach(() => {
+    game = new Game([[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]);
+  });
   test('Should create game object with 3 arrays as the properties', () => {
-    const game = new Game([[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]);
+    game = new Game([[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]],[[1,2,3],[1,2,3]]);
     expect(game.colArray).toEqual([[1,2,3],[1,2,3]]);
     expect(game.rowArray).toEqual([[1,2,3],[1,2,3]]);
     expect(game.boxArray).toEqual([[1,2,3],[1,2,3]]);
+  });
+  test('Should return whether user input is "legal" or not', () => {
+    const legal =  game.isLegal();
+    expect(legal === false);
   });
 });
