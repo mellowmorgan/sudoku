0c8ca7fa003421eeb523352c0eb549dea9d38ac1
mellowmorgan <morgan.waites@gmail.com>
2021-11-09 20:48:17 -0800

    add ui logic make array of ids

--- src/index.js
+++ src/index.js
@@ -6,10 +6,39 @@ import './css/styles.css';
 
 $("button").on("click", function(event){
   event.preventDefault();
+  let array = [];
   for(let i=1;i<10;i++){
     for(let i2=1;i2<10;i2++){
        let s= i.toString()+i2.toString();
-       console.log(s);
+        array.push(s);
   }}
+  //alert(array);
+  let counter=1;
+  let box=1;
+  let externalCounter=1;
+  let extraExtCounter=1;
+  for(let i=0;i<array.length;i++){
+    if (externalCounter===10){
+      box=box-3;
+      externalCounter=1;
+    }
+    if (extraExtCounter===28){
+      box=4;
+    }
+    if (extraExtCounter===56){
+      box=7;
+    }
+    if(counter===4){
+      box++;
+      counter=1;
+    }
+    array[i]=array[i]+box;
+    extraExtCounter++;
+    counter++;
+    externalCounter++;
+  }
+  alert(array);
 
 });
+
+
