2393ec212139a315eff24d9d932087538dad7c13
mellowmorgan <morgan.waites@gmail.com>
2021-11-09 23:29:51 -0800

    WIP

--- src/index.js
+++ src/index.js
@@ -18,16 +18,18 @@ $("button").on("click", function(event){
   let externalCounter=1;
   let extraExtCounter=1;
   for(let i=0;i<array.length;i++){
-    if (externalCounter===10){
+    if (extraExtCounter===56){
+      box=6;
+      externalCounter=1;
+    }
+    if (externalCounter===10 && extraExtCounter!==56){
       box=box-3;
       externalCounter=1;
     }
     if (extraExtCounter===28){
-      box=4;
+      box=3;
     }
-    if (extraExtCounter===56){
-      box=7;
-    }
+    
     if(counter===4){
       box++;
       counter=1;
@@ -37,7 +39,66 @@ $("button").on("click", function(event){
     counter++;
     externalCounter++;
   }
+  let  userArrayNumbers = [];
+  
+
+  array.forEach(function(stringId){
+    let value;
+    if ($("#"+stringId).is("input")){
+      
+      value = $("#"+stringId).val();
+    }
+    else{
+       value = $("#"+stringId).text();
+    }
+    userArrayNumbers.push(parseInt(value));
+  });
   alert(array);
+  function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+        const chunk = arr.slice(i, i + chunkSize);
+        res.push(chunk);
+    }
+    return res;
+}
+  let arrayOfRows=sliceIntoChunks(userArrayNumbers,9);
+ 
+  function transposeArray(array, arrayLength){
+    let newArray = [];
+    for(let i = 0; i < array.length; i++){
+        newArray.push([]);
+    };
+
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < arrayLength; j++){
+            newArray[j].push(array[i][j]);
+        };
+    };
+
+    return newArray;
+}
+  let arrayOfColumns= transposeArray(arrayOfRows,9);
+ // alert(arrayOfColumns[0]);
+
+  // let columnCounter=0
+  // let rowArrays=[];
+  // let rowArray=[];
+  // for(let i=0;i<userArrayNumbers.length;i++){
+  //   if(columnCounter<9){
+  //   rowArray.push(userArrayNumbers[i]);}
+  //   if(columnCounter===8){
+  //     rowArrays.push(rowArray);
+  //     columnCounter=0;
+  //     rowArray=[];
+  //   }
+  //   columnCounter++;
+  // }
+  // alert(rowArrays[0]);
+  // alert(rowArrays[1]);
+  // alert(rowArrays[2]);
+  
+
 
 });
 
